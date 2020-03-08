cd0a43bd34b7e8a010905ec7ca3e8d88fee4c53d
Senthil Kumar Gundepalli -X (sgundepa - WIPRO LIMITED at Cisco) <sgundepa@cisco.com>
2020-03-08 18:40:17 +0530

    updated header portion

--- js/agency.js
+++ js/agency.js
@@ -29,9 +29,9 @@
   // Collapse Navbar
   var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
+     // $("#mainNav").addClass("navbar-shrink");
     } else {
-      $("#mainNav").removeClass("navbar-shrink");
+     // $("#mainNav").removeClass("navbar-shrink");
     }
   };
   // Collapse now if page is not at top
